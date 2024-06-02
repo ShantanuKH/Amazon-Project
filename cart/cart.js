6ce7160a255df06d40df05c1fbe8ed3e37c36c91
ShantanuKH <khadseshantanu02@gmail.com>
2024-06-02 18:59:29 +0530

    Create HTML for order summary

--- cart/cart.js
+++ cart/cart.js
@@ -1,4 +1,17 @@
-export const cart=[];
+export const cart=[{
+    // Here we are using a technique called normalizing the data means with the peoductId itself we can get other information abot the product
+    productId:
+    'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2,
+},{
+    productId:
+    '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:1
+}
+
+
+
+];
 // Here, we are exporting the variable that we want to get out
 
 
